2732b9b3ea85043b554b006187e14b46db7a12f7
b191832Anand <151979845+b191832Anand@users.noreply.github.com>
2024-04-03 18:35:19 +0530

    Create include.cpp

---
+++ include.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define vi vector<int>
+#define vc vector<char>
+#define pb(x) push_back(x)
+int mod=1e9+7;
+signed main(){
+    
+}
