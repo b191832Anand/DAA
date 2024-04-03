ee6815cac511da8c5e6a59d6d501f32d714c4324
b191832Anand <151979845+b191832Anand@users.noreply.github.com>
2024-04-03 18:32:07 +0530

    Add files via upload

---
+++ solve.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define int long long
+#define all(x) x.begin(),x.end()
+#define vi vector<int>
+#define vc vector<char>
+#define pb(x) push_back(x)
+int max(int a,int b){return a>b?a:b;}
+int min(int a,int b){return a<b?a:b;}
+int mod=1e9+7;
+void solve(){
+	
+}
+int32_t main(){
+	int t;cin>>t;
+	while(t--)solve();
+}
