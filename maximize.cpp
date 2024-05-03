380c00f28e2a2624ff71254132685966056bebe4
b191832Anand <151979845+b191832Anand@users.noreply.github.com>
2024-05-03 20:26:04 +0530

    Add files via upload

---
+++ maximize.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define umii unordered_map<int,int>
+#define si set<int>
+#define sc set<char>
+#define all(x) x.begin(),x.end()
+#define ALL(x) x.rbegin(),x.rend()
+#define ln "\n"
+
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+template <class T>
+void print_v(vector<T> &v) { for (auto x : v) cout<<x<<" ";cout<<endl;}
+template <class T>
+void input_v(vector<T> &v) {for(auto &i:v)cin>>i;}
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+void solve(){
+   int x;cin>>x;
+   int val=0,y=0;
+   for(int i=1;i<x;i++){
+      int cnt=(gcd(x,i)+i);
+      if(cnt>=val){
+         val=cnt;
+         y=i;
+      }
+   }
+   cout<<y<<ln;
+}
+int main()
+{
+   int t;cin>>t;
+   while(t--)solve();
+}
